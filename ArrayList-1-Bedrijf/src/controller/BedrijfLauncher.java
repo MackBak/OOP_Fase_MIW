cf55e4bd3fd6033c95470f306ebff4d4956e46c1
MackBak <154362357+MackBak@users.noreply.github.com>
2024-05-22 15:07:07 +0200

    Exercise 1 - Chapter 6

---
+++ ArrayList-1-Bedrijf/src/controller/BedrijfLauncher.java
@@ -0,0 +1,52 @@
+package controller;
+
+import Model.Afdeling;
+import Model.Persoon;
+import Model.Werknemer;
+import Model.Zzper;
+
+import java.util.ArrayList;
+
+public class BedrijfLauncher {
+    public static void main(String[] args) {
+
+        int aantalAfdelingen = 4;
+        Afdeling[] afdelingen = new Afdeling[aantalAfdelingen];
+        afdelingen[0] = new Afdeling("Uitvoering", "Hilversum");
+        afdelingen[1] = new Afdeling("Support", "Amsterdam");
+        afdelingen[2] = new Afdeling("Management", "Almere");
+        afdelingen[3] = new Afdeling("Documentatie", "Gouda");
+
+        Werknemer baas = new Werknemer("Mark", "Den Haag", afdelingen[2], 10000.00);
+        Werknemer medewerker = new Werknemer("Caroline", "Delft", afdelingen[1], 4000.00);
+        Zzper assistent = new Zzper("Klaas", "Diemen", afdelingen[3], 50.0);
+        Zzper projectleider = new Zzper("Ronald", "Zaandam", afdelingen[0], 80.0);
+
+        assistent.huurIn(160);
+        projectleider.huurIn(320);
+
+        ArrayList<Persoon> personen = new ArrayList<Persoon>();
+        personen.add(new Werknemer("Mark", "Den Haag", afdelingen[2], 10000.0));
+        personen.add(new Werknemer("Angelique", "Rotterdam", afdelingen[2], 5000.0));
+        personen.add(new Werknemer("Caroline", "Delft", afdelingen[1], 4000.0));
+        personen.add(new Zzper("Klaas", "Diemen", afdelingen[3], 50.00));
+        personen.add(new Zzper("Ronald", "Zaandam", afdelingen[0], 80.00));
+        personen.add(new Zzper("Jannie", "Utrecht", afdelingen[0], 60.00));
+        personen.add(new Zzper("Anne", "Zwolle", afdelingen[0], 40.00));
+
+        for (int teller = 0; teller < personen.size(); teller++) {
+            if (personen.get(teller) instanceof Zzper) {
+                ((Zzper) personen.get(teller)).huurIn(320);
+            }
+        }//for loop to gather all Zzpers in the ArrayList.
+
+        for (int i = 0; i < personen.size(); i++) {
+            toonJaarInkomen(personen.get(i));
+        }
+
+    }//end of main
+
+    public static void toonJaarInkomen(Persoon persoon) {
+        System.out.println(persoon.getNaam() + " verdient " + persoon.berekenJaarInkomen() + " per jaar.");
+    }
+}
